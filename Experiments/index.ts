da54eeed0713b8f6eb7f2bde244aef06ed731c3e
janisslsm <97699850+janisslsm@users.noreply.github.com>
2022-10-08 20:36:03 +0300

    Proper experiment enabling

--- Experiments/index.ts
+++ Experiments/index.ts
@@ -1,21 +1,31 @@
 import { Plugin } from "aliucord/entities";
-import { getByProps, UserStore } from "aliucord/metro";
+import { getByProps, UserStore, FluxDispatcher } from "aliucord/metro";
 
 export default class Experiments extends Plugin {
-    public async start() {
-        this.commands.registerCommand({
-            name: "experiments",
-            description: "Enables Experiments",
-            options: [],
-            execute: (args, ctx) => {
-                var actions = getByProps('_actionHandlers')._actionHandlers["_orderedActionHandlers"]["CONNECTION_OPEN"];
-                var user = UserStore.getCurrentUser();
-                actions.find(n => n.name === "ExperimentStore").actionHandler({
-                    type: "CONNECTION_OPEN", user: { flags: user.flags |= 1 }, experiments: [],
-                });
-                actions.find(n => n.name === "DeveloperExperimentStore").actionHandler();
-                user.flags &= ~1; "done";
-            }
+    public enableExperiments() {
+        var actions = getByProps('_actionHandlers')._actionHandlers["_orderedActionHandlers"]["CONNECTION_OPEN"];
+        var user = UserStore.getCurrentUser();
+        actions.find(n => n.name === "ExperimentStore").actionHandler({
+            type: "CONNECTION_OPEN", user: { flags: user.flags |= 1 }, experiments: [],
         });
+        actions.find(n => n.name === "DeveloperExperimentStore").actionHandler();
+        user.flags &= ~1;
+    }
+    
+    public async start() {
+        if (UserStore.getCurrentUser())
+            this.enableExperiments()
+        else {
+            try {
+                const handleConnect = () => {
+                    FluxDispatcher.unsubscribe("CONNECTION_OPEN", handleConnect);
+                    this.enableExperiments()
+                }
+
+                FluxDispatcher.subscribe("CONNECTION_OPEN", handleConnect);
+            } catch (error) {
+                this.logger.error((error as Error).stack)
+            }
+        }
     }
 }
