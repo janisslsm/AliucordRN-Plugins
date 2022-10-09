aa5feea4def84d29c68e95d8f193835c97c4205b
janisslsm <97699850+janisslsm@users.noreply.github.com>
2022-10-09 16:50:07 +0300

    Indent everything equally

--- FreeNitro/index.ts
+++ FreeNitro/index.ts
@@ -3,29 +3,29 @@ import { getByProps } from 'aliucord/metro';
 import { after, before, callOriginal } from "aliucord/utils/patcher";
 
 const [{ getEmojiURL }, usability, { getChannel }, Messages] = [
-  getByProps('getEmojiURL'),
-  getByProps('canUseEmojisEverywhere', 'canUseAnimatedEmojis'),
-  getByProps('getChannel'),
-  getByProps('receiveMessage', 'sendMessage'),
+    getByProps('getEmojiURL'),
+    getByProps('canUseEmojisEverywhere', 'canUseAnimatedEmojis'),
+    getByProps('getChannel'),
+    getByProps('receiveMessage', 'sendMessage'),
 ];
 
 type Emoji = {
-  roles: any[];
-  require_colons: boolean;
-  name: string;
-  managed: boolean;
-  id: string;
-  available: boolean;
-  animated: boolean;
-  url: string;
-  allNamesString: string;
-  guildId: string;
+    roles: any[];
+    require_colons: boolean;
+    name: string;
+    managed: boolean;
+    id: string;
+    available: boolean;
+    animated: boolean;
+    url: string;
+    allNamesString: string;
+    guildId: string;
 };
 
 export default class FreeNitro extends Plugin {
     public async start() {
-        after(usability, 'canUseEmojisEverywhere', (ctx) => { ctx.result = true; });
-        after(usability, 'canUseAnimatedEmojis', (ctx) => { ctx.result = true; });
+        after(usability, 'canUseEmojisEverywhere', (ctx) => ctx.result = true);
+        after(usability, 'canUseAnimatedEmojis', (ctx) => ctx.result = true);
         before(Messages, 'sendMessage', (ctx) => {
             const [channelId, message] = ctx.args;
 
